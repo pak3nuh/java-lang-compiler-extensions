48b80bdcd2522b9516639dcf0f036f6ea2e72513
Nuno Caro <nuno.pik@gmail.com>
2020-09-15 17:30:22 +0100

    feature Moves branch selection to switch
    Its cleaner and improves performance.

--- enum-expression/processor/src/main/kotlin/pt/pak3nuh/util/lang/enum_expression/processor/ExpressionWriters.kt
+++ enum-expression/processor/src/main/kotlin/pt/pak3nuh/util/lang/enum_expression/processor/ExpressionWriters.kt
@@ -6,11 +6,9 @@ import com.squareup.javapoet.JavaFile
 import com.squareup.javapoet.MethodSpec
 import com.squareup.javapoet.ParameterSpec
 import com.squareup.javapoet.ParameterizedTypeName
-import com.squareup.javapoet.TypeName
 import com.squareup.javapoet.TypeSpec
 import com.squareup.javapoet.TypeVariableName
-import java.nio.file.Paths
-import java.util.Objects
+import java.util.*
 import java.util.function.Supplier
 import javax.annotation.processing.Filer
 import javax.lang.model.element.Modifier
@@ -24,7 +22,7 @@ class ExpressionWriter(val filer: Filer) {
                 .addTypeVariable(variableName)
                 .addMethods(toInterfaceMethods(enumData, variableName))
                 .addMethod(evaluator(enumData, className))
-                .addMethod(lambdaExhaustive(enumData, className))
+                .addMethod(lambdaExhaustive(enumData))
                 .build()
         val file = JavaFile.builder(enumData.pkg, type).build()
         file.writeTo(filer)
@@ -38,23 +36,22 @@ class ExpressionWriter(val filer: Filer) {
                 .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
                 .addParameter(enumData.enumType, "value")
                 .addParameter(ParameterSpec.builder(expressionType, "delegate").build())
-                .addCode(evaluatorCode(enumData.symbols, enumData.enumType))
+                .addCode(evaluatorCode(enumData.symbols){ "delegate.$it()" })
                 .returns(variableName)
                 .build();
     }
 
-    private fun evaluatorCode(symbols: List<String>, enumType: ClassName): CodeBlock {
+    private fun evaluatorCode(symbols: List<String>, symbolFn: (String) -> String): CodeBlock {
         val objectsClassName = ClassName.get(Objects::class.java)
         return CodeBlock.builder()
-                .addStatement("\$T.requireNonNull(value)", objectsClassName)
+                .beginControlFlow("switch(\$T.requireNonNull(value))", objectsClassName)
                 .apply {
                     symbols.forEach {
-                        beginControlFlow("if (\$T.$it.equals(value))", enumType)
-                        addStatement("return delegate.$it()")
-                        endControlFlow()
+                        addStatement("case $it: return ${symbolFn(it)}")
                     }
                 }
-                .addStatement("throw new IllegalStateException(String.valueOf(value))")
+                .addStatement("default: throw new IllegalStateException(String.valueOf(value))")
+                .endControlFlow()
                 .build()
     }
 
@@ -67,29 +64,14 @@ class ExpressionWriter(val filer: Filer) {
         }
     }
 
-    private fun lambdaExhaustive(enumData: EnumData, expressionName: ClassName): MethodSpec {
+    private fun lambdaExhaustive(enumData: EnumData): MethodSpec {
         val variableName = TypeVariableName.get("W")
         return MethodSpec.methodBuilder("eval")
                 .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
                 .addTypeVariable(variableName)
                 .addParameters(toParameters(enumData.symbols, variableName, enumData.enumType))
-                .addCode(lambdaCode(enumData.symbols, expressionName, variableName))
+                .addCode(evaluatorCode(enumData.symbols){ "$it.get()" })
                 .returns(variableName)
-                .build()
-    }
-
-    private fun lambdaCode(symbols: List<String>, expressionName: ClassName, variableName: TypeVariableName): CodeBlock {
-        return CodeBlock.builder()
-                .beginControlFlow("return eval(value, new \$T<\$T>() ", expressionName, variableName)
-                .apply {
-                    symbols.forEach {
-                        addLine("@Override")
-                        beginControlFlow("public \$T \$L()", variableName, it)
-                                .addStatement("return \$L.get()", it)
-                        endControlFlow()
-                    }
-                }
-                .endControlFlow(")")
                 .build()
     }
 
@@ -100,8 +82,4 @@ class ExpressionWriter(val filer: Filer) {
             ParameterSpec.builder(supplierType, it).build()
         }
     }
-}
-
-private fun CodeBlock.Builder.addLine(format: String, vararg args: Any): CodeBlock.Builder {
-    return this.add("$format${System.lineSeparator()}", args)
 }
